c652ecc8710bab9a44141383b8eecad346302035
keirrisson44 <ra24351835850@acaonsfatima.org.br>
2025-11-08 01:22:46 -0300

    adicionei mais

--- src/router.jsx
+++ src/router.jsx
@@ -4,6 +4,7 @@ import Complementar from './pages/complementar/index.jsx';
 import Denuncia1 from './pages/denuncia1/index.jsx';
 import Denuncia2 from './pages/denuncia2/index.jsx';
 import DenunciaEnviada from './pages/denunciaEnviada/index.jsx';
+import Adimin from './pages/adimin/index.jsx';
 
 export default function Navegacao() {
 
@@ -15,6 +16,7 @@ export default function Navegacao() {
         <Route path='/denuncia2' element={ <Denuncia2/> } />
         <Route path='/denunciaEnviada' element={ <DenunciaEnviada/>} />
         <Route path='/complementar' element={ <Complementar/>} />
+        <Route path='/Adimin' element={ <Adimin/>} />
         </Routes>
         </BrowserRouter>
     )
